6a5b4e3ed79d59575397a8d6b676d68eb51e86db
Uiltemberg <trader.uds@gmail.com>
2026-04-27 13:00:02 -0300

    fix: chart dimensions error on dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -301,9 +301,9 @@ export const Dashboard: React.FC = () => {
               </div>
             </div>
           </div>
-          <div className="h-[350px] w-full min-h-[350px]">
+          <div className="h-[350px] w-full min-h-[350px] relative">
             {chartData.length > 0 && (
-              <ResponsiveContainer width="100%" height="100%">
+              <ResponsiveContainer width="100%" height="100%" minWidth={0}>
                 <BarChart data={chartData} margin={{ top: 10, right: 10, left: 0, bottom: 0 }}>
                   <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#f4f4f5" />
                   <XAxis 
